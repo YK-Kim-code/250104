e4d64742180367907c1468c985eaac8b58103781
YK-Kim-code <youky0508@gmail.com>
2025-01-04 10:35:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import requests
 fav_country = 'India'
 
 
-type_info = pandas.read_csv('/kaggle/input/mbtitypes-full/types.csv', index_col='Type')
-df = pandas.read_csv('/kaggle/input/mbtitypes-full/countries.csv', index_col='Country')
+type_info = pandas.read_csv('countriesMBTI.csv', index_col='Type')
+df = pandas.read_csv('countriesMBTI.csv', index_col='Country')
 pandas.options.display.max_columns = 32
 df.loc[[fav_country]] * 100
