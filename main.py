3dfe5f8362c30373b4ab174b0e3b081ac659e9b9
YK-Kim-code <youky0508@gmail.com>
2025-01-04 10:42:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ st.write("데이터 로드 끝")
 
 # 국가 선택
 st.subheader("Select a Country")
-available_countries = df["country"].unique()
+available_countries = df["Country"].unique()
 selected_country = st.selectbox("Choose a country:", available_countries)
 
 # 데이터 필터링
-filtered_data = df[df["country"] == selected_country]
+filtered_data = df[df["Country"] == selected_country]
 
 # MBTI 분포 계산
 mbti_distribution = filtered_data["mbti"].value_counts().reset_index()
