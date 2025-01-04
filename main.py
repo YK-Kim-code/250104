ad078eb15976d2e7076dc57cca16c8b749603997
YK-Kim-code <youky0508@gmail.com>
2025-01-04 10:55:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,7 @@ filtered_data = df[df["Country"] == selected_country]
 st.subheader(f"MBTI Distribution in {selected_country}")
 st.table(filtered_data)
 
-# MBTI 분포 계산
-mbti_counts = filtered_data["MBTI"].value_counts()
 
 # 막대그래프 시각화
 st.subheader(f"MBTI Distribution Chart in {selected_country}")
-st.bar_chart(mbti_counts)
+st.bar_chart(filtered_data)
